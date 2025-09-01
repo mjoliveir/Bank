9ae7a70f1f691482be6d5dc271ae03521cdc8215
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-09-01 09:23:31 -0300

    mudando o modificador de acesso de alguns atributos da classe para melhor manuseio do código

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,8 +1,8 @@
 export class Negociacao {
   constructor(
     private readonly _data: Date,
-    private readonly quantidade: number,
-    private readonly valor: number
+    private readonly _quantidade: number,
+    private readonly _valor: number
   ) {}
    
   //getter do volume
@@ -14,5 +14,13 @@ export class Negociacao {
   get data(): Date {
     const data = new Date(this._data)
     return data
+  }  
+  
+  get quantidade(): number {
+    return this._quantidade
+  }  
+  
+  get valor(): number {
+    return this._valor
   }
 }
