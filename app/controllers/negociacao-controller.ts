d3eab4dbe306d22fb9ab4a87fa20072c98e04709
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-09-01 09:22:45 -0300

    Estrutura da lógica de foco de campo após a limpeza dos campos. adição de elementos de negociacao num array de negociacoes e update desse array a cada nova negociação

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -1,23 +1,20 @@
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
+import { NegociacoesView } from "../views/negociacoes-view.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
   private negociacoes = new Negociacoes;
+  private negociacoesView = new NegociacoesView('#negociacoesView')
+
 
   constructor() {
     this.inputData = document.querySelector("#data");
     this.inputQuantidade = document.querySelector("#quantidade");
     this.inputValor = document.querySelector("#valor");
-  }
-
-  adiciona(): void {
-    const negociacao = this.criaNegociacao();
-    this.negociacoes.adiciona(negociacao); 
-    console.log(this.negociacoes.lista());
-    this.limparFormulario();
+    this.negociacoesView.update(this.negociacoes) // renderiza e atualiza o estado do DOM
   }
 
   criaNegociacao(): Negociacao {
@@ -28,11 +25,23 @@ export class NegociacaoController {
     return new Negociacao(date, quantidade, valor);
   }
 
-  limparFormulario(): void {
+    limparFormulario(): void {
     console.log("O formulário foi limpo");
     this.inputData.value = "";
     this.inputValor.value = "";
     this.inputQuantidade.value = "";
-    this.inputData.focus();
+    this.inputData.focus(); // esse comando volta o foco para o primeiro campo de formulário
   }
+
+  adiciona(): void {
+    const negociacao = this.criaNegociacao(); //chama a função criaNegociacao()
+    this.negociacoes.adiciona(negociacao); 
+    console.log(this.negociacoes.lista()); // mostra a lista na tela
+    this.limparFormulario();
+    this.negociacoesView.update(this.negociacoes)
+  }
+
+
+
+
 }
