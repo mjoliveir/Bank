4413edc6c038e31e988955e55966280776c36961
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-09-01 09:25:42 -0300

    import de negociacoesView

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,6 @@
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
+import { NegociacoesView } from "./views/negociacoes-view.js";
+const negociacoesView = new NegociacoesView('#negociacoesView');
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
 form.addEventListener('submit', (event) => {
