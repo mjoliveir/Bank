60ae96e5be4f8172abe23ab47f32346fef7483c1
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-09-03 16:26:34 -0300

    abstração de classes e mettodos, gerenciamento de acesso a métodos

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,11 +1,16 @@
-export class View<T>{
+export abstract class View<T>{ //a classe abstrata imposibilita o usuário de criar instancias dessa classe
 
-  protected elemento: HTMLElement;
-  constructor(seletor: string) {
+  protected elemento: HTMLElement; //protected protege a classe como private mas permite que as filhas acessem seus metodos
+  constructor(seletor: string) { 
     this.elemento = document.querySelector(seletor);
   }
 
-  template(model: T): string {
-    throw Error('a classe filha deve implementar seu proprio template') 
-  }
+    update(model: T) : void{
+        const template = this.template(model)
+        console.log(template)
+        this.elemento.innerHTML = this.template(model)
+    }
+
+   abstract template(model: T): string //abstract function, isso obriga a classe que herda essa classe a implementar esse metodo template
+
 }
