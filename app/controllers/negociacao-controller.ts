33a7f5ff6e2758208c361e9132bd957f7c1f4306
Matheus Jesus de Oliveira <32xxthunderxx32@gmail.com>
2025-09-05 09:46:35 -0300

    adição de abstração para lógica de dia util

--- app/controllers/negociacao-controller.ts
+++ app/controllers/negociacao-controller.ts
@@ -2,23 +2,33 @@ import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
+import { diasDaSemana } from "../enums/diasDaSemana.js";
 
 export class NegociacaoController {
   private inputData: HTMLInputElement;
   private inputQuantidade: HTMLInputElement;
   private inputValor: HTMLInputElement;
-  private negociacoes = new Negociacoes;
-  private negociacoesView = new NegociacoesView('#negociacoesView')
-  private mensagemView = new MensagemView('#mensagemView')
+  private negociacoes = new Negociacoes();
+  private negociacoesView = new NegociacoesView("#negociacoesView");
+  private mensagemView = new MensagemView("#mensagemView");
 
   constructor() {
     this.inputData = document.querySelector("#data");
     this.inputQuantidade = document.querySelector("#quantidade");
     this.inputValor = document.querySelector("#valor");
-    this.negociacoesView.update(this.negociacoes) // renderiza e atualiza o estado do DOM
+    this.negociacoesView.update(this.negociacoes); // renderiza e atualiza o estado do DOM
   }
 
-  criaNegociacao(): Negociacao {
+  private diaUtil(data: Date): boolean {
+    if (data.getDay( ) > diasDaSemana.DOMINGO && data.getDay() < diasDaSemana.SABADO) {
+      console.log('o dia é útil')
+      return true
+    } else {console.log('fim de semana')
+      return false
+    }
+  }
+
+  private criaNegociacao(): Negociacao {
     const exp = /-/g; // A string passada pelo construtor deve ter o ano, mês e dia separados por vírgula.
     const date = new Date(this.inputData.value.replace(exp, ","));
     const quantidade = parseInt(this.inputQuantidade.value);
@@ -26,7 +36,7 @@ export class NegociacaoController {
     return new Negociacao(date, quantidade, valor);
   }
 
-    limparFormulario(): void {
+  private limparFormulario(): void {
     console.log("O formulário foi limpo");
     this.inputData.value = "";
     this.inputValor.value = "";
@@ -34,16 +44,21 @@ export class NegociacaoController {
     this.inputData.focus(); // esse comando volta o foco para o primeiro campo de formulário
   }
 
-  adiciona(): void {
+  public adiciona(): void {
     const negociacao = this.criaNegociacao(); //chama a função criaNegociacao()
-    this.negociacoes.adiciona(negociacao); 
-    console.log(this.negociacoes.lista()); // mostra a lista na tela
-    this.limparFormulario();
-    this.negociacoesView.update(this.negociacoes)
-    this.mensagemView.update('Sua negociação foi cadastrada com sucesso!!')
+    if (!this.diaUtil(negociacao.data)) {
+      this.mensagemView.update("Somente negociações em dias úteis são aceitas.");
+    }else{
+      this.negociacoes.adiciona(negociacao);
+      console.log(this.negociacoes.lista()); // mostra a lista na tela
+      this.limparFormulario();
+      this.atualizaView();
+
+    }
   }
 
-
-
-
+  private atualizaView() {
+    this.negociacoesView.update(this.negociacoes);
+    this.mensagemView.update("Sua negociação foi cadastrada com sucesso!!");
+  }
 }
